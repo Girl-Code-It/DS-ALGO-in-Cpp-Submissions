4b053f6c5eda27e52e3a7f871bbbebb678a3f639
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-06-13 12:16:23 +0530

    Update Diamond of numbers.cpp

--- kanakmilestone-1/patternpractice/Diamond of numbers.cpp
+++ kanakmilestone-1/patternpractice/Diamond of numbers.cpp
@@ -13,7 +13,7 @@ int main() {
 	        cout<<p;
 	     cout<<""<<endl;      
 	}
-	for(int i=n/2;i>0;i--)
+	for(int i=n/2;i>=0;i--)
 	{    for(int p=i;p<=n/2;p++)
 	        cout<<" ";
 	    for(int p=1;p<=2*i+1;p++)
