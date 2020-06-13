39a08c615ea957f06564e30e4fc2d0b225fc6eb7
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-06-13 10:35:32 +0530

    Create Pattern_ify.cpp

---
+++ kanakmilestone-1/patternpractice/Pattern_ify.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    for(int j=n-i;j>0;j--)
+	    {
+	        cout<<"* ";
+	    }
+	    cout<<""<<endl;
+	}
+	return 0;
+}
