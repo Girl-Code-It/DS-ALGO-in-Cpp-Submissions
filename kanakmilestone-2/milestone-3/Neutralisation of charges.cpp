681303d625f778ece5b81d73574a6c86a08d53e0
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-06-23 18:32:19 +0530

    Update Neutralisation of charges.cpp

--- kanakmilestone-2/milestone-3/Neutralisation of charges.cpp
+++ kanakmilestone-2/milestone-3/Neutralisation of charges.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 int main() {
-	int num,count,i;
+	long int num,count,i;
 	cin >> num;		
 	string s,k;
 	k="";
 	cin>>s;
 	char a=s[0];
 	count=0;
+	long int p=0;
 	for(i=0;i<s.length();i++)
 	{
 		if(s[i]==a)
@@ -19,41 +20,41 @@ int main() {
 		else
 		{
 			if(count%2==1)
-			k+=s[i-1];
-			a=s[i];
-			count=1;
+			{	if(a!=k[k.length()-1])
+				k+=s[i-1];
+				//cout<<k<<"ppp"<<endl;
+				a=s[i];
+				count=1;
+			}else{
+				if(s[i-1]==k[k.length()-1])
+				k=k.substr(0,k.length()-1);
+				a=k[k.length()-1];
+			//	cout<<a<<endl;
+				count=1;
+				i--;
+			}
 
 		}
 	}
 	if(count%2==1)
-			k+=s[i-1];
-			a=s[i];
-			count=1;
-
-	s="";
-	a=k[0];
-	count=0;
-	for(i=0;i<k.length();i++)
-	{
-		if(k[i]==a)
-		count++;
-		else
-		{
-			if(count%2==1)
-			s+=k[i-1];
-			a=k[i];
-			count=1;
-
-		}
-	}
-	if(count%2==1)
-			s+=k[i-1];
-			a=k[i];
-			count=1;
+			{	if(a!=k[k.length()-1])
+				k+=s[i-1];
+				//cout<<k<<"ppp"<<endl;
+				a=s[i];
+				count=1;
+			}else{
+				if(s[i-1]==k[k.length()-1])
+				k=k.substr(0,k.length()-1);
+				a=k[k.length()-1];
+			//	cout<<a<<endl;
+				count=1;
+				i--;
+			}
 	
-	cout<<s.length()<<endl;
-	for(int i=0;i<s.length();i++)
-	cout<<s[i];
+	
+	cout<<k.length()<<endl;
+	for(long int i=0;i<k.length();i++)
+	cout<<k[i];
 	
 	return 0;
 	}								// Reading input from 
