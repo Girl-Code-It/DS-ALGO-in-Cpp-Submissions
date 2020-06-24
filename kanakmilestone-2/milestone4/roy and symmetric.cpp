15d2ad87a77f232ef7c3a3d5cc0e082c71a712d2
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-06-24 12:25:44 +0530

    Update roy and symmetric.cpp

--- kanakmilestone-2/milestone4/roy and symmetric.cpp
+++ kanakmilestone-2/milestone4/roy and symmetric.cpp
@@ -4,12 +4,6 @@
 using namespace std;
 
 int main() {
-
-
-//partial solved
-
-
-
 		int t;
 		cin>>t;
 		while(t--)
@@ -20,47 +14,20 @@ int main() {
 			{
 				cin>>a[i];
 			}
-			for(int i=0;i<n/2;i++)
+			for(int i=0;i<n;i++)
 			{
-				for(int j=0;j<n/2;j++)
+				for(int j=0;j<n;j++)
 				{
 					if((a[i][j]==a[n-i-1][j])&&(a[i][j]==a[i][n-1-j]))
 						int h=0;
 					else
 					{
 						cout<<"NO"<<endl;
-						//cout<<a[i][j]<<" "<<a[n-1-i][j]<<" "<<a[i][n-1-j]<<endl;
-						//cout<<i<<" "<<j<<endl;
 						goto q;
 					}
 				}
 			}
-			if(n%2==1)
-			{	
-				int i=n/2;
-				for(int j=0;j<n/2;j++)
-				{
-					if(a[i][j]==a[i][n-1-j])
-					int h=0;
-					else
-					{
-						cout<<"NO"<<endl;
-						goto q;
-					}
-				}
-				i=n/2;
-				for(int j=0;j<n/2;j++)
-				{
-					if(a[j][i]==a[n-1-j][i])
-					int h=0;
-					else
-					{
-						cout<<"NO"<<endl;
-						goto q;
-					}
-				}
-
-			}
+		
 			cout<<"YES"<<endl;
 			q:
 			int r=0;
