b03ca0be883a2b40d375116e518b2b678444e7cb
Kashika Jain <56075265+kashika3101@users.noreply.github.com>
2020-12-28 13:43:02 +0530

    Create MiddleElement_LinkedList.cpp

---
+++ Kashika/LinkedList/MiddleElement_LinkedList.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+    ListNode* temp= head;
+        int count=0;
+        while(temp!=NULL)
+        {
+            temp=temp->next;
+            count++;
+        }
+        for(int i=0;i<count/2;i++)
+        {
+            head=head->next;
+        }
+        return head;
+    }
+};
